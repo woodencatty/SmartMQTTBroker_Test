b4517f8fcf76b145e2424d2531676043757ce288
WoodenCatty <WoodenCatty@gmail.com>
2019-08-07 20:34:32 +0900

    work?

--- TestCase1/case SB/Sensor.js
+++ TestCase1/case SB/Sensor.js
@@ -3,14 +3,20 @@ var client  = mqtt.connect('mqtt://192.168.1.140:1883')
 var qs = require('querystring');
 var count = 0;
 
-client.on('connect', function () {
+var sender = "IoTSensor01"
 
+client.on('connect', function () {
+  client.subscribe('/DataOnReturn_'+sender, function (err) {
+    if (!err) {
+      console.log("Subscribe Complete")
+    }
+  })
   var sendMessage = setInterval(() => {
 
     timeNow = new Date().getTime();
-    client.publish('/Data', '{"count" : ' + count + ', "timesent" : ' + timeNow + ', "sender" : "IoTSensor01"}')
+    client.publish('/Data', '{"count" : ' + count + ', "timesent" : ' + timeNow + ', "sender" : '+sender+'}')
     count++;
-    if (count > 100) {
+    if (count > 1000) {
       clearInterval(sendMessage);
       client.end();
       setTimeout(() => {
@@ -18,6 +24,18 @@ client.on('connect', function () {
       }, 100);
     }
   }, 100)
+})
+
+client.on('message', function (topic, message) {
+  timeNow = new Date().getTime();
+
+  var data = JSON.parse(message.toString());
+
+  console.log('Message Arrived from '+data.sender +' and Return to '+data.sender + ' : ' + '{"count" : ' + data.count + ', "timesent" : ' + data.timesent + ', "sender" : '+data.sender+'}');
+  // console.log(timeNow + "-" + data.timesent);
+  console.log(data.sender + "has Time Easped Try "+ data.count +" in : "+ (data.timesent - timeNow) +"("+ data.timesent+"-"+timeNow+")");
+
+
 })
 
 
