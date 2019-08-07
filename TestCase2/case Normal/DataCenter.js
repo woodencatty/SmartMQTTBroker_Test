bef886af1dfefe06bdee71f16a27aaad0731b942
WoodenCatty <WoodenCatty@gmail.com>
2019-08-07 21:37:33 +0900

    ww

--- TestCase2/case Normal/DataCenter.js
+++ TestCase2/case Normal/DataCenter.js
@@ -1,6 +1,19 @@
 const http = require('http');
-var os 	= require('os-utils');
 var qs = require('querystring');
+var cpuStat = require('cpu-stat');
+
+
+cpuStat.usagePercent(function(err, percent, seconds) {
+  if (err) {
+    return console.log(err);
+  }
+
+  //the percentage cpu usage over all cores
+  console.log(percent);
+
+  //the approximate number of seconds the sample was taken over
+  console.log(seconds);
+});
 
 http.createServer(function (request, response) {
 
